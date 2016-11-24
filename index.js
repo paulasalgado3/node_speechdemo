381a416b031db78236549de4043dffebe32ea3d2
Paula Salgado <mail@algo.com>
2016-11-24 17:19:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var https = require('https');
+var WebSocketServer = require('ws').Server;
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var serverConfig = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt'),
+};
+
+var app = express();
+var HTTPS_PORT = 8443;
+
+var httpsServer = https.createServer(serverConfig, app).listen(HTTPS_PORT);
+
+var wss = new WebSocketServer({server: httpsServer});
+var wss_externo ;
+wss.on('connection', function(wss) {
+	wss_externo=wss;
+	wss.send('conectado');
+    wss.on('message', function(message) {
+		enviarMensaje(message);
+    });
+	
+});
+
+function enviarMensaje(mensaje){
+ wss.clients.forEach(function each(client){
+			console.log(mensaje);
+                        client.send(mensaje);
+                });
+};
+//wss.broadcast = function(data) {
+//    for(var i in this.clients) {
+//        this.clients[i].send(data);
+//    }
+//};
+
+app.get(/^(.+)$/, function(req, res){ 
+    switch(req.params[0]) {
+        case '/prueba.html':
+            res.send("prueba ok");
+            break;
+	case '/derecha':
+		wss_externo.send('derecha');
+		break;
+	case '/izquierda':
+		wss_externo.send('izquierda');
+		break;
+    	default: res.sendFile( __dirname + req.params[0]); 
+    }
+ });
+
+
+console.log('Servidor corriendo');
